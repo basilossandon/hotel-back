ed2c5fabadef5abd139a942c801fc68eb0c62650
basil <basilossandon@gmail.com>
2019-05-22 18:36:54 -0400

    v1.1.1 Package name fix

--- src/main/java/teamgeso/hotelback/HotelbackApplication.java
+++ src/main/java/teamgeso/hotelback/HotelbackApplication.java
@@ -1,4 +1,4 @@
-package com.teamgeso.hotelback;
+package teamgeso.hotelback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/teamgeso/hotelback/ServletInitializer.java
+++ src/main/java/teamgeso/hotelback/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.teamgeso.hotelback;
+package teamgeso.hotelback;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
