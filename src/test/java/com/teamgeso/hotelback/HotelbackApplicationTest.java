b5dc58e686b3739040eeb1b49dc16e7c7df50147
Pedro <pedro.aguilar.p@usach.cl>
2019-05-24 16:35:35 -0400

    test1

---
+++ src/test/java/com/teamgeso/hotelback/HotelbackApplicationTest.java
@@ -0,0 +1,14 @@
+package com.teamgeso.hotelback;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HotelbackApplicationTest {
+
+    @Test
+    public void main() {
+        fail("exception");
+
+    }
+}
