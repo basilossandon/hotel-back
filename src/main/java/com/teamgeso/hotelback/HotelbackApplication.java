cce77fc2e8c2e713f1a63d7dcc4dcc78a2d879c3
basilossando <basil.ossandon@usach.cl>
2019-05-24 20:52:18 -0400

    v1.4.5 component scan updated

--- src/main/java/com/teamgeso/hotelback/HotelbackApplication.java
+++ src/main/java/com/teamgeso/hotelback/HotelbackApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan("com.teamgeso.hotelback.repository")
+@ComponentScan(basePackages = { "com.teamgeso.hotelback.repository","com.teamgeso.hotelback.controller"} )
 public class HotelbackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HotelbackApplication.class, args);
--- src/main/java/com/teamgeso/hotelback/controller/RoomController.java
+++ src/main/java/com/teamgeso/hotelback/controller/RoomController.java
@@ -1,19 +1,14 @@
 package com.teamgeso.hotelback.controller;
 import com.teamgeso.hotelback.model.Room;
 import com.teamgeso.hotelback.repository.RoomRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Validated
 @CrossOrigin(origins = "*")
 public class RoomController {
     @Autowired
