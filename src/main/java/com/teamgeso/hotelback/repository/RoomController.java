264d99c4cf0ce7ae52cd936c4148f4dc9e5a8edc
basilossando <basil.ossandon@usach.cl>
2019-05-24 20:31:23 -0400

    v1.4.2 Refactor final

--- src/main/java/com/teamgeso/hotelback/repository/RoomController.java
+++ src/main/java/com/teamgeso/hotelback/repository/RoomController.java
@@ -3,12 +3,13 @@ import com.teamgeso.hotelback.model.Room;
 import com.teamgeso.hotelback.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
+
 @CrossOrigin(origins = "*")
 public class RoomController {
     @Autowired
