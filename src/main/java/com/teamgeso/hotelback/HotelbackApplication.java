0ec3d812082841a3c27df7c4ec24b58fa57d685d
basil <basilossandon@gmail.com>
2019-05-28 02:05:47 -0400

    v1.9 WebSecurityConfig added

--- src/main/java/com/teamgeso/hotelback/HotelbackApplication.java
+++ src/main/java/com/teamgeso/hotelback/HotelbackApplication.java
@@ -2,14 +2,19 @@ package com.teamgeso.hotelback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+
 @ComponentScan(basePackages = { "com.teamgeso.hotelback.repository","com.teamgeso.hotelback.controller"} )
 public class HotelbackApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(HotelbackApplication.class, args);
+
+
 	}
 
 }
