26c62bad45aba06a3a8a11d853dc367002dd3967
AlejandroOrostica <alejandro.orostica@usach.cl>
2019-05-28 01:22:51 -0400

    v1.8.1 delete and update methods for roomType added

--- src/main/java/com/teamgeso/hotelback/controller/RoomTypeController.java
+++ src/main/java/com/teamgeso/hotelback/controller/RoomTypeController.java
@@ -7,6 +7,7 @@ import com.teamgeso.hotelback.repository.RoomTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -40,6 +41,26 @@ public class RoomTypeController implements DaoRoomType {
         roomTypeRepository.save(roomTypeToSave);
         return roomTypeRepository.findById(roomTypeToSave.getId());
     }
+
+    @PostMapping (value = "/{id}")
+    public @ResponseBody
+    Optional<RoomType> updateRoomType(@PathVariable Integer id, @RequestBody RoomTypeDTO roomType){
+        RoomType updatedRoomType = new RoomType();
+        updatedRoomType.setName(roomType.getName());
+        updatedRoomType.setPrice(roomType.getPrice());
+        updatedRoomType.setId(id);
+        roomTypeRepository.save(updatedRoomType);
+        return roomTypeRepository.findById(updatedRoomType.getId());
+    }
+
+    @PostMapping (value = "/{id}/delete")
+    public @ResponseBody
+    List<RoomType> deleteRoomType(@PathVariable Integer id){
+        roomTypeRepository.deleteById(id);
+        return roomTypeRepository.findAll();
+    }
+
+
 
 
 
