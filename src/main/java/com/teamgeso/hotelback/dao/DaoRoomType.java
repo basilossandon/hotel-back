f22fc2664cb661bae1248ac35ca872d3c55ed981
eliass1997ok <gabriel.gaete.l@usach.cl>
2019-05-28 04:03:14 -0400

    v1.9.6 Fix on code smell. Unused import removed.

--- src/main/java/com/teamgeso/hotelback/dao/DaoRoomType.java
+++ src/main/java/com/teamgeso/hotelback/dao/DaoRoomType.java
@@ -3,7 +3,6 @@ package com.teamgeso.hotelback.dao;
 import com.teamgeso.hotelback.dto.RoomTypeDTO;
 import com.teamgeso.hotelback.model.RoomType;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface DaoRoomType {
